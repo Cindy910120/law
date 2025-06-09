34419f4d7134d2f1d1f73750e6901f70105490dd
Cindy910120 <106473129+Cindy910120@users.noreply.github.com>
2025-06-09 16:25:00 +0800

    最後頁面

---
+++ pages/labor-law/sexual-harassment-prevention.vue
@@ -0,0 +1,14 @@
+<template>
+  <LawArticleDisplay 
+    page-title="性騷擾防治區" 
+    :articles="sexualHarassmentPreventionArticles"
+  />
+</template>
+
+<script setup lang="ts">
+import { sexualHarassmentPreventionArticles } from '~/data/sexual-harassment-prevention.ts';
+</script>
+
+<style scoped>
+/* 可添加專區特定樣式 */
+</style>
